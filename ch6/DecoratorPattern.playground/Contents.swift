bc7e733d189ed971f2f5c564be9eb557339a313c
roger.wang[王濬淇] <roger.wang@104.com.tw>
2017-09-27 18:27:11 +0800

    ch 6 commit

--- ch6/DecoratorPattern.playground/Contents.swift
+++ ch6/DecoratorPattern.playground/Contents.swift
@@ -1,5 +1,76 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import Foundation
 
-var str = "Hello, playground"
+//Can't modify
+class Hero {
+    var hp: Int = 10
+    
+    func showHP() {
+        print(hp)
+    }
+    
+}
+
+class StrongHero: Hero {
+    override func showHP() {
+        print(hp + 10)
+    }
+}
+
+class LittleHero: Hero {
+    override func showHP() {
+        print(hp - 5)
+    }
+}
+
+// ...ect
+
+//Method 2: extension & protocal
+protocol JobHero {
+    var hp: Int { get }
+    var jobName: String { get }
+    func showJobNameAndHP()
+}
+
+class BaseHero: JobHero {
+    var hp: Int {
+        get {
+            return 10
+        }
+    }
+    var jobName: String {
+        get {
+            return "BaseHero"
+        }
+    }
+    
+    func showJobNameAndHP() {
+        print("\(jobName) : \(hp)")
+    }
+}
+
+class HeroDecorator: JobHero {
+    var hp: Int {
+        get {
+            return heroInstance.hp
+        }
+    }
+    
+    var jobName: String {
+        get {
+            return heroInstance.jobName
+        }
+    }
+    
+    let heroInstance: JobHero
+    
+    required init(jobHero: JobHero) {
+        self.heroInstance = jobHero
+    }
+    
+    func showJobNameAndHP() {
+        print("\(jobName) : \(hp)")
+    }
+}
